e556e621bdbdfcfa8bef2f470917b8dc76b2e972
HECTOR FERNANDEZ <hector@fernandez-pinacho.com>
2023-03-02 19:07:33 +0100

    H: Made the Comment appear and disappear

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -7,6 +7,14 @@ import './style/Create.css'
 
 export default class Create extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            isToggled: true,
+        }
+        this.formRef = React.createRef()
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
         const inputField = document.getElementById('comment-input');
@@ -32,22 +40,27 @@ export default class Create extends React.Component {
           });
       }
 
+    toggleDisplay = () => {
+        const form = this.formRef.current;
+        form.classList.toggle('show-form');
+        this.setState(prevState => ({ isToggled: !prevState.isToggled }));
+    }
 
     render() {
         return (
             <div className="create-wrapper">
-                <button className="comment-button">
+                <button onClick={this.toggleDisplay} className="comment-button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
                 </button>
-                <form onSubmit={this.handleSubmit}>
+                <form ref={this.formRef} onSubmit={this.handleSubmit}>
                     <div className='comment-wrapper'>
-                    <div className='user'>
-                        <a className='profile-pic' href='#'><img src='src/Assets/profilepic.jpg'/></a>
-                        <span className='username'>{/* TODO: Person username*/}@User</span>
-                    </div>
-                    <div className="comment">
-                        <input type="text" id="comment-input" className="input-comment" placeholder="This is your comment"></input>
-                    </div>
+                        <div className='user'>
+                            <a className='profile-pic' href='#'><img src='src/Assets/profilepic.jpg'/></a>
+                            <span className='username'>{/* TODO: Person username*/}@User</span>
+                        </div>
+                        <div className="comment">
+                            <input type="text" id="comment-input" className="input-comment" placeholder="This is your comment"></input>
+                        </div>
                     </div>
                     <button type="submit">Submit</button>
                 </form>
