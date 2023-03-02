b0a00daa03c464398296d49ad5e8ab1068a72197
HECTOR FERNANDEZ <hector@fernandez-pinacho.com>
2023-03-02 18:17:51 +0100

    H: added a circle to click in create

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,6 +2,7 @@ import './style/comment.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import './style/Create.css'
 
 
 export default class Create extends React.Component {
@@ -34,18 +35,23 @@ export default class Create extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className='comment-wrapper'>
-                <div className='user'>
-                    <a className='profile-pic' href='#'><img src='src/Assets/profilepic.jpg'/></a>
-                    <span className='username'>{/* TODO: Person username*/}@User</span>
-                </div>
-                <div className="comment">
-                    <input type="text" id="comment-input" className="input-comment" placeholder="This is your comment"></input>
-                </div>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            <div className="create-wrapper">
+                <button className="comment-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+                </button>
+                <form onSubmit={this.handleSubmit}>
+                    <div className='comment-wrapper'>
+                    <div className='user'>
+                        <a className='profile-pic' href='#'><img src='src/Assets/profilepic.jpg'/></a>
+                        <span className='username'>{/* TODO: Person username*/}@User</span>
+                    </div>
+                    <div className="comment">
+                        <input type="text" id="comment-input" className="input-comment" placeholder="This is your comment"></input>
+                    </div>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         );
     }
 }
